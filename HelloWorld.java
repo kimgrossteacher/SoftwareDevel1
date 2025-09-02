c1e217b95e7bc0e8876a6ebec89dee5ea8760f1e
kimgrossteacher <148360456+kimgrossteacher@users.noreply.github.com>
2025-09-02 10:24:10 -0600

    Create HelloWorld.java
    First try

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//This is my first program
+// By Mr Gross   kim.gross@malad.us
+//this is a hello world
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("I'm Mr Gross and am 58 years old");
+    }
+    
+}
