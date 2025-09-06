ae0ab3edfc27772bf260f785c1b3395d04614819
kimgrossteacher <148360456+kimgrossteacher@users.noreply.github.com>
2025-09-05 21:08:12 -0600

    Create Hello.java
    Sample for students having problems.

---
+++ Hello.java
@@ -0,0 +1,17 @@
+// program by kim.gross@malad.us
+//this is an example hello world program for my Sd1 class
+//program prints out hello to my family
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is Mr Gross");
+        System.out.println("I am married and we have 8 kids between us.");
+        String dad="Bud";
+        String mom="Patty";
+        System.out.println("My parents names are "+dad+" and "+mom);
+        System.out.println("We have 16 grandkids");
+
+    }
+
+}
