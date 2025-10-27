f4ccae5d73cd2602ee13d9702299742c77629e08
kimgrossteacher <148360456+kimgrossteacher@users.noreply.github.com>
2025-10-27 15:00:01 -0600

    ts

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int age=15;
+        String name = "Ryker";
+        System.out.println("Hello World I am "+name);
+        System.out.println("I am "+age+" years old");
+        
+    }
+
+}
